77a5e417082573baeb40af4b790653fe355abf3d
YzfJengthen <1500825146@qq.com>
2019-01-27 17:21:25 +0800

    Add files via upload

--- Assets/OBJImport/Samples/ObjFromFile.cs
+++ Assets/OBJImport/Samples/ObjFromFile.cs
@@ -5,32 +5,38 @@ using UnityEngine;
 
 public class ObjFromFile : MonoBehaviour
 {
-    public String path = @"C:\Users\vive_1\Desktop\zf\mesh100";
+    public String path = @"C:\Users\vive_1\Desktop\zf\mesh";
     public String[] files;
+    public String[] mtls;
     public OBJLoader loadedObj = new OBJLoader();
+    public OBJLoader loadedObj1 = new OBJLoader();
     public int i=0;
     public GameObject temp;
+    public GameObject temp1;
     void Start()
     {
+
         temp = new GameObject();
         files = Directory.GetFiles(path, "*.obj");
-        temp = loadedObj.Load(files[0]);
-        Destroy(temp);
-        temp = loadedObj.Load(files[90]);
+        mtls = Directory.GetFiles(path, "*.mtl");
+
     }
 
-    /***
+   
     void Update()
     {
-        
+
         if (i < files.Length && i > 0)
         {
             Destroy(temp);
-            temp = loadedObj.Load(files[i]);
+            Destroy(loadedObj.Load(files[i-1], mtls[i-1]));
+            temp = loadedObj.Load(files[i], mtls[i]);
+            temp.transform.localScale = new Vector3(-0.1f, 0.1f, 0.1f);
         }
         else if (i == 0)
         {
-            temp = loadedObj.Load(files[i]);
+            temp = loadedObj.Load(files[i], mtls[i]);
+            temp.transform.localScale = new Vector3(-0.1f, 0.1f, 0.1f);
         }
         else if (i == files.Length)
         {
@@ -38,6 +44,6 @@ public class ObjFromFile : MonoBehaviour
         }
         i++;
     }
-    **/
+
 
 }
